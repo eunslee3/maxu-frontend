c78b00ebdeffcbae09531730f70d6101c281f7a8
sam-lee-ku <sam.lee@ku.edu>
2024-07-08 20:56:32 -0500

    camera capture styling

--- pages/utils/Camera.js
+++ pages/utils/Camera.js
@@ -1,9 +1,9 @@
 import { CameraView, useCameraPermissions } from 'expo-camera';
 import React, { useState } from 'react';
-import { Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View, StyleSheet, Button } from 'react-native';
 
 const Camera = () => {
-  const [facing, setFacing] = useState('back');
+  const [facing, setFacing] = useState('front');
   const [permission, requestPermission] = useCameraPermissions();
 
   const toggleCameraFacing = () => {
@@ -23,6 +23,8 @@ const Camera = () => {
     <View style={styles.container}>
       <CameraView style={styles.camera} facing={facing}>
         <View style={styles.buttonContainer}>
+          <TouchableOpacity style={[styles.capture]}>
+          </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
             <Text style={styles.text}>Flip Camera</Text>
           </TouchableOpacity>
@@ -42,14 +44,22 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
-    flexDirection: 'row',
-    backgroundColor: 'transparent',
-    margin: 64,
+    position: 'relative',
+    justifyContent: 'flex-end',
+    alignItems: 'center'
   },
   button: {
     flex: 1,
     alignSelf: 'flex-end',
     alignItems: 'center',
+  },
+  capture: {
+    position: 'absolute',
+    bottom: 20,
+    width: 50,
+    height: 50,
+    borderRadius: 100,
+    backgroundColor: 'white'
   },
   text: {
     fontSize: 24,
