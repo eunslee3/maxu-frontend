e9e9295a9fbe47dd0e7bee376064568d13efccd7
unknown <lee.eunseong7856@gmail.com>
2024-07-08 21:57:20 -0500

    camera wasn't working so I c/p the documentation. need to create ways to capture

--- pages/utils/Camera.js
+++ pages/utils/Camera.js
@@ -1,37 +1,41 @@
 import { CameraView, useCameraPermissions } from 'expo-camera';
-import React, { useState } from 'react';
-import { Text, TouchableOpacity, View, StyleSheet, Button } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 
-const Camera = () => {
-  const [facing, setFacing] = useState('front');
+export default function App() {
+  const [facing, setFacing] = useState('back');
   const [permission, requestPermission] = useCameraPermissions();
 
-  const toggleCameraFacing = () => {
+  if (!permission) {
+    // Camera permissions are still loading.
+    return <View />;
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={styles.container}>
+        <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission" />
+      </View>
+    );
+  }
+
+  function toggleCameraFacing() {
     setFacing(current => (current === 'back' ? 'front' : 'back'));
   }
 
-  console.log(permission)
-
-  if (!permission?.granted) {
-    <View style={styles.container}>
-      <Text style={{ textAlign: 'center' }}>We need your permission to show the camera</Text>
-      <Button onPress={requestPermission} title="grant permission" />
-    </View>
-  }
-  
   return (
     <View style={styles.container}>
       <CameraView style={styles.camera} facing={facing}>
         <View style={styles.buttonContainer}>
-          <TouchableOpacity style={[styles.capture]}>
-          </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={toggleCameraFacing}>
             <Text style={styles.text}>Flip Camera</Text>
           </TouchableOpacity>
         </View>
       </CameraView>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -44,22 +48,14 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
-    position: 'relative',
-    justifyContent: 'flex-end',
-    alignItems: 'center'
+    flexDirection: 'row',
+    backgroundColor: 'transparent',
+    margin: 64,
   },
   button: {
     flex: 1,
     alignSelf: 'flex-end',
     alignItems: 'center',
-  },
-  capture: {
-    position: 'absolute',
-    bottom: 20,
-    width: 50,
-    height: 50,
-    borderRadius: 100,
-    backgroundColor: 'white'
   },
   text: {
     fontSize: 24,
@@ -67,5 +63,3 @@ const styles = StyleSheet.create({
     color: 'white',
   },
 });
-
-export default Camera
